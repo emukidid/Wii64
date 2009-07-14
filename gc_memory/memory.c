7638e2e4ea0f7fbb4409f73c254283221203c18c
emukidid <emukidid@gmail.com>
2009-07-14 02:33:15 +0000

    Ported latest /tehpola/r4300 core, but with blocks in MEM2. Default settings in the Makefile.GLN64_dev_wii are expansion pak enabled and 8MB recompCache.

--- gc_memory/memory.c
+++ gc_memory/memory.c
@@ -1001,7 +1001,7 @@ void read_nomemd()
 void write_nomem()
 {
    if (!interpcore && !invalid_code_get(address>>12))
-     if (blocks[address>>12]->code_addr[(address&0xFFF)/4])
+     //if (blocks[address>>12]->code_addr[(address&0xFFF)/4])
        invalid_code_set(address>>12, 1);
    address = virtual_to_physical_address(address,1);
    if (address == 0x00000000) return;
@@ -1011,7 +1011,7 @@ void write_nomem()
 void write_nomemb()
 {
    if (!interpcore && !invalid_code_get(address>>12))
-     if (blocks[address>>12]->code_addr[(address&0xFFF)/4])
+     //if (blocks[address>>12]->code_addr[(address&0xFFF)/4])
        invalid_code_set(address>>12, 1);
    address = virtual_to_physical_address(address,1);
    if (address == 0x00000000) return;
@@ -1021,7 +1021,7 @@ void write_nomemb()
 void write_nomemh()
 {
    if (!interpcore && !invalid_code_get(address>>12))
-     if (blocks[address>>12]->code_addr[(address&0xFFF)/4])
+     //if (blocks[address>>12]->code_addr[(address&0xFFF)/4])
        invalid_code_set(address>>12, 1);
    address = virtual_to_physical_address(address,1);
    if (address == 0x00000000) return;
@@ -1031,7 +1031,7 @@ void write_nomemh()
 void write_nomemd()
 {
    if (!interpcore && !invalid_code_get(address>>12))
-     if (blocks[address>>12]->code_addr[(address&0xFFF)/4])
+     //if (blocks[address>>12]->code_addr[(address&0xFFF)/4])
        invalid_code_set(address>>12, 1);
    address = virtual_to_physical_address(address,1);
    if (address == 0x00000000) return;
