a173ab459fd7b6e4b98b1f14f58b4dc0f0753587
emu_kidid <emukidid@gmail.com>
2021-08-20 10:02:06 +0930

    - WiiVC mode, don't apply default count_per_op as 1 even when overclocked. - Add boxart.bin

--- main/main_gc-menu2.cpp
+++ main/main_gc-menu2.cpp
@@ -312,7 +312,7 @@ int main(int argc, char* argv[]) {
 	count_per_op	 = COUNT_PER_OP_3;
 	screenMode		 = SCREENMODE_4x3;
 #else
-	count_per_op	 = SYS_GetCoreMultiplier() < 5.0 ? COUNT_PER_OP_2 : COUNT_PER_OP_1;
+	count_per_op	 = COUNT_PER_OP_2;
 	screenMode		 = CONF_GetAspectRatio() == CONF_ASPECT_16_9 ? SCREENMODE_16x9_PILLARBOX : SCREENMODE_4x3;
 #endif
 	videoMode		 = VIDEOMODE_AUTO;
